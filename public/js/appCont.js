212fd7c64e0ae5f86c37513cfd059a098e154760
monaj <monsarka@in.ibm.com>
2016-07-05 16:25:41 +0530

    update

---
+++ public/js/appCont.js
@@ -0,0 +1,20 @@
+var batapp = angular.module('batapp', ['ngRoute','ngResource']); 
+    
+	batapp.config(['$routeProvider',function( $routeProvider) {
+        $routeProvider
+            // route for the home page
+            .when('/', {
+                templateUrl : 'pages/scenario.html',
+                controller  : 'mainController'
+            })
+            .otherwise({ 
+            		//	redirectTo: '/signup' 
+            });
+    }]);
+    
+	batapp.controller('mainController',['$scope', '$route', '$resource', '$http', '$location', function($scope, $route, $resource, $http, $location) {
+    	    	
+    }]);
+    
+    
+    
