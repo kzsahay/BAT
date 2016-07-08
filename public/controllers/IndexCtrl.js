7579c12117a1ef1120ca31c1a45b40129ed16f1f
monaj <monsarka@in.ibm.com>
2016-07-08 17:39:07 +0530

    update left navigation

--- public/controllers/IndexCtrl.js
+++ public/controllers/IndexCtrl.js
@@ -2,55 +2,30 @@ var IndexCtrl = angular.module('IndexCtrl', []);
 
 IndexCtrl.controller('IndexCtrl', ['$scope','$location','$http','$rootScope',
   function ($scope, $location,$http,$rootScope) {
+	$scope.selectedClass = 'target';
 	
-	$scope.page1 = 'clock1';
-	$scope.page2 = 'new2';
-	$scope.page3 = 'pig2';
-	$scope.page4 = 'chick2';
-	$scope.page5 = 'chick3';
-	
-	$scope.gotoPage1 = function(){
-		$scope.page1 = 'clock1';
-		$scope.page2 = 'new2';
-		$scope.page3 = 'pig2';
-		$scope.page4 = 'chick2';
-		$scope.page5 = 'chick3';
+	$scope.gotoPage1 = function() {
+		$scope.selectedClass = 'target';
 		$location.path('/page1');
 	}
 		
 	$scope.gotoPage2 = function (){
-		$scope.page1 = 'clock2';
-		$scope.page2 = 'new1';
-		$scope.page3 = 'pig2';
-		$scope.page4 = 'chick2';
-		$scope.page5 = 'chick3';
+		$scope.selectedClass = 'pricing';
 		$location.path('/page2');
 	}
 	
 	$scope.gotoPage3 = function (){
-		$scope.page1 = 'clock2';
-		$scope.page2 = 'new2';
-		$scope.page3 = 'pig1';
-		$scope.page4 = 'chick2';
-		$scope.page5 = 'chick3';
+		$scope.selectedClass = 'account';
 		$location.path('/page3');
 	}
 	
 	$scope.gotoPage4 = function (){
-		$scope.page1 = 'clock2';
-		$scope.page2 = 'new2';
-		$scope.page3 = 'pig2';
-		$scope.page4 = 'chick2';
-		$scope.page5 = 'chick3';
+		$scope.selectedClass = 'competitor';
 		$location.path('/page4');
 	}
 	
 	$scope.gotoPage5 = function (){
-		$scope.page1 = 'clock1';
-		$scope.page2 = 'new2';
-		$scope.page3 = 'pig2';
-		$scope.page4 = 'chick2';
-		$scope.page5 = 'chick3';
+		$scope.selectedClass = '';
 		$location.path('/page5');
 	}
 	
