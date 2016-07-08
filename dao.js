e2b9cdff070575ab2eb3260d15ea4b9930127a3c
riteshhalder <riteshhalder@riteshs-mbp.in.ibm.com>
2016-07-08 14:34:08 +0530

    load table api

--- dao.js
+++ dao.js
@@ -9,41 +9,87 @@ exports.getTableDetails = function(conn, viewData, req) {
 
 		var tabledata = {"accountName": "", "marketshareForecasts": []};
 		var marketshareForecasts = {"scenario":"", "priceScenario": [], "mshareTrend": []};
-		var pricescenario = {"brandName": "", "brandPrice": ""};
+		
 		var mshareTrend = {"weekNum": "", "mshare": ""};
-		var arraynew = [];
+		
+		var k = 0; m =0;
 
-		var stmt1 = daoConst.BRANDFETCH;
+		var stmt1 = "select * from PRICINGA";
 		var priceS = conn.querySync(stmt1);
 		var priceS_data = JSON.parse(JSON.stringify(priceS));
-		 console.log("pricescenario -->" + JSON.stringify(priceS_data));
+		// console.log("pricescenario -->" + JSON.stringify(priceS_data));
 		
-//		marketshareForecasts.scenario = "Base";
-//		for (var i in priceS_data) {
-//			pricescenario.brandName = priceS_data[i].BrandSegment;
-//			pricescenario.brandPrice = priceS_data[i].Price_Mean;
-//			
-//			console.log(pricescenario)
-//			arraynew.push(pricescenario);
-//			// if(i>0){
-//			// 	if(arraynew[i].brandName == arraynew[i-1].brandName){
-//			// 		arraynew.splice(i);
-//			// 	}
-//			// }
-//			
-//		}
-//			console.log(arraynew)
-//			marketshareForecasts.priceScenario = arraynew;
-//		
-//		tabledata.accountName = "VALORA";
-//		tabledata.marketshareForecasts.push(marketshareForecasts)
+		for (var i in priceS_data) {
+			if(priceS_data[i].Account == "VALORA    "){
+				tabledata.accountName = "VALORA";
 
+				if(priceS_data[i].TYPE == "B "){
 
-		// for (var i in locationdetail_data) {
-		// 	detailsobj.Location[i] = locationdetail_data[i].CITY;
+					marketshareForecasts.scenario = "Base"
+					var pricescenario = {"brandName": "", "brandPrice": ""};
+					var arraynew = [];
+
+					pricescenario.brandName = priceS_data[i].BrandSegment;
+					pricescenario.brandPrice = priceS_data[i].Price_Mean;
+
+					marketshareForecasts.priceScenario[k++] = pricescenario;
+					// if(i>0){
+					// 	if(priceS_data[i].BrandSegment == priceS_data[i-1].BrandSegment){
+					// 		marketshareForecasts.priceScenario.splice(k-1, 1);
+					// 	}
+					// }
+
+					var mshareTrend = {"weekNum": "", "mshare": ""};
+					mshareTrend.weekNum = i;//priceS_data[i].$TI_TimeLabel;
+					mshareTrend.mshare = priceS_data[i].Share_Sum;
+					console.log(mshareTrend)
+					marketshareForecasts.mshareTrend[k++] = mshareTrend;
+
+					
+					tabledata.marketshareForecasts.push(marketshareForecasts)
+
+				}
+
+				
+
+				// if(priceS_data[i].TYPE == "C "){
+
+				// 	marketshareForecasts.scenario = "Corporate Price"
+				// 	var pricescenario = {"brandName": "", "brandPrice": ""};
+				// 	var arraynew = [];
+
+				// 	pricescenario.brandName = priceS_data[i].BrandSegment;
+				// 	pricescenario.brandPrice = priceS_data[i].Price_Mean;
+
+				// 	marketshareForecasts.priceScenario[k++] = pricescenario;
+				// 	// if(i>0){
+				// 	// 	if(priceS_data[i].BrandSegment == priceS_data[i-1].BrandSegment){
+				// 	// 		marketshareForecasts.priceScenario.splice(k-1, 1);
+				// 	// 	}
+				// 	// }
+
+				// 	var mshareTrend = {"weekNum": "", "mshare": ""};
+				// 	mshareTrend.weekNum = i;//priceS_data[i].$TI_TimeLabel;
+				// 	mshareTrend.mshare = priceS_data[i].Share_Sum;
+
+				// 	marketshareForecasts.mshareTrend[k++] = mshareTrend;
+
+				// 	tabledata.marketshareForecasts.push(marketshareForecasts)
+
+				// }
+			}
+		}
+		
+		// for(var i in marketshareForecasts.priceScenario){
+		// 	if(marketshareForecasts.priceScenario[i]!= undefined){
+		// 		marketshareForecasts.priceScenario[m++] = marketshareForecasts.priceScenario[i];
+		// 	}
 		// }
 		
-		viewData.Data = priceS_data;
+		
+
+
+		viewData.Data = tabledata;
 
 	} catch (err) {
 		console.log("Error in getTableDetails :: "
