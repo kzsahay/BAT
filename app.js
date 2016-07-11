feb1f9c7d387edb3c8a66216f2894f82f7b5fcd9
riteshhalder <riteshhalder@riteshs-mbp.in.ibm.com>
2016-07-11 13:08:54 +0530

    changes

--- app.js
+++ app.js
@@ -38,13 +38,13 @@ vcapCredential.setVCAP_Parameters(process.env.VCAP_SERVICES);
 
 //-------------------------CORS support----------------------------------//
 
-//app.use(function(req, res, next) {
-//	res.header("Access-Control-Allow-Origin", "*");
-//	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//	res.header("Access-Control-Allow-Headers",
-//			"Origin, X-Requested-With, Content-Type, Accept");
-//	next();
-//});
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.header("Access-Control-Allow-Headers",
+			"Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
 
 //Function for performing Read Operation from DB
 function readDB(Select_Query) {
