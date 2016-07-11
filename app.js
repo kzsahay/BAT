375213481138cc9ae40c1caec03873438bbdfa43
riteshhalder <riteshhalder@riteshs-mbp.in.ibm.com>
2016-07-11 13:16:06 +0530

    changes

--- app.js
+++ app.js
@@ -41,8 +41,7 @@ vcapCredential.setVCAP_Parameters(process.env.VCAP_SERVICES);
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-	res.header("Access-Control-Allow-Headers",
-			"Origin, X-Requested-With, Content-Type, Accept");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
 });
 
