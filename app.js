af6974d3b892520b385c31d23aee1c8184e61ab2
riteshhalder <riteshhalder@riteshs-mbp.in.ibm.com>
2016-07-11 13:36:15 +0530

    changes

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ function GetDBCon() {
 
 //loading db tables slide 6
 app.get('/loadtable', function(req, res) {
-	console.log(req.headers['Access-Control-Allow-Headers'])
+	
 	if(typeof(req.headers['Access-Control-Allow-Headers']) === 'undefined'){
         req.headers['Access-Control-Allow-Headers'] = "Origin, X-Requested-With, Content-Type, Accept";
     }
@@ -125,6 +125,11 @@ app.get('/loadtable', function(req, res) {
 
 //finalize button slide 6
 app.post('/finalize', function(req, res) {
+	
+	if(typeof(req.headers['Access-Control-Allow-Headers']) === 'undefined'){
+        req.headers['Access-Control-Allow-Headers'] = "Origin, X-Requested-With, Content-Type, Accept";
+    }
+	console.log(req.headers['Access-Control-Allow-Headers'])
 	var error_msg = "";
 	var conn = util.getDBConn();
 	try {
@@ -160,6 +165,11 @@ app.post('/finalize', function(req, res) {
 
 //run scenario button slide 6
 app.post('/runscenario', function(req, res) {
+	
+	if(typeof(req.headers['Access-Control-Allow-Headers']) === 'undefined'){
+        req.headers['Access-Control-Allow-Headers'] = "Origin, X-Requested-With, Content-Type, Accept";
+    }
+	console.log(req.headers['Access-Control-Allow-Headers'])
 	var error_msg = "";
 	var conn = util.getDBConn();
 	try {
