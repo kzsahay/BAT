9f00ef75ddbb32c68a4f5fe148e342016838ce6d
shailesh <ADMINIBM@ADMINIB-AUIIJMJ>
2016-07-12 22:21:38 +0530

    configurations for unit tests

--- e2etest/protractor.conf.js
+++ e2etest/protractor.conf.js
@@ -9,20 +9,19 @@ exports.config = {
 
   multiCapabilities: [{
     'browserName': 'chrome',
-	'name': ' BAT Login Test -Chrome'
+	'name': 'BAT Login Test -Chrome'
   },
 	{
     'browserName': 'firefox',
 	'name': ' BAT Login Test Firefox'
   }],
-	sauceUser: 'shaileshagarwal',
-	sauceKey: '54e88738-b884-443f-844e-4b5fe95f23d5',
+  sauceUser: 'shaileshagarwal',
+  sauceKey: '54e88738-b884-443f-844e-4b5fe95f23d5',
 
   baseUrl: 'http://batobacco.mybluemix.net/',
 
-  framework: 'jasmine'
+  framework: 'jasmine' ,
   jasmineNodeOpts: {
     defaultTimeoutInterval: 50000
   }
-
 };
