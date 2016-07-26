2b5e099bfb89ad9f36ba0682a4674b740884dd38
Harsimerjit Singh <harsimes@in.ibm.com>
2016-07-26 14:13:52 +0000

    Suraj changes drill down

--- public/controllers/Page0Ctrl.js
+++ public/controllers/Page0Ctrl.js
@@ -229,7 +229,324 @@ $scope.linechart=$(function () {
         }]
     });
 		
-		
+    $(function () {
+        // Create the chart
+        $('#container').highcharts({
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: 'Brand contribution across my accounts'
+            },
+            subtitle: {
+                text: 'Click the columns to view details.'
+            },
+            xAxis: {
+                type: 'category'
+            },
+            yAxis: {
+                title: {
+                    text: 'Total percent market share'
+                }
+
+            },
+            legend: {
+                enabled: false
+            },
+            plotOptions: {
+                series: {
+                    borderWidth: 0,
+                    dataLabels: {
+                        enabled: true,
+                        format: '{point.y:.1f}%'
+                    }
+                }
+            },
+
+            tooltip: {
+                headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+                pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+            },
+
+            series: [{
+                name: 'Manufacturers',
+                colorByPoint: true,
+                data: [{
+                    name: 'BAT',
+                    y: 37.40,
+                    drilldown: 'BAT'
+                }, {
+                    name: 'Phillip Morris',
+                    y: 16.50,
+                    drilldown: 'Ph Morris'
+                }, {
+                    name: 'JTI',
+                    y: 41.40,
+                    drilldown: 'JTI'
+                }, {
+                    name: 'Others',
+                    y: 4.70,
+                    drilldown: 'Others'
+                }]
+            }],
+            drilldown: {
+                series: [{
+                    name: 'BAT',
+                    id: 'BAT',
+                    data: [{
+                        name: 'DAVIDOFF',
+                        y: 0.70,
+                        drilldown: 'DAVIDOFF'
+                    }, {
+                        name: 'DUNHILL',
+                        y: 0.30,
+                        drilldown: 'DUNHILL'
+                    }, {
+                        name: 'FRANCAISE',
+                        y: 0.10,
+                        drilldown: 'FRANCAISE'
+                    }, {
+                        name: 'JPS',
+                        y: 0.00,
+                        drilldown: 'JPS'
+                    }, {
+                        name: 'KENT',
+                        y: 5.70,
+                        drilldown: 'KENT'
+                    }, {
+                        name: 'VOGUE',
+                        y: 1.70,
+                        drilldown: 'VOGUE'
+                    }, {
+                        name: 'SELECT',
+                        y: 4.70,
+                        drilldown: 'SELECT'
+                    }, {
+                        name: 'LUCKY STRIKE',
+                        y: 1.20,
+                        drilldown: 'LUCKY STRIKE'
+                    }, {
+                        name: 'GAULOISES BLONDES',
+                        y: 1.00,
+                        drilldown: 'GAULOISES BLONDES'
+                    }, {
+                        name: 'JPGAULOISES TRADS',
+                        y: 0.60,
+                        drilldown: 'GAULOISES TRAD'
+                    }, {
+                        name: 'GITANES',
+                        y: 0.10,
+                        drilldown: 'GITANES'
+                    }, {
+                        name: 'MAROCAINE',
+                        y: 1.10,
+                        drilldown: 'MAROCAINE'
+                    }, {
+                        name: 'MARYLONG',
+                        y: 1.70,
+                        drilldown: 'MARYLONG'
+                    }, {
+                        name: 'PALL MALL',
+                        y: 2.70,
+                        drilldown: 'PALL MALL'
+                    }, {
+                        name: 'PARISIENNE',
+                        y: 18.40,
+                        drilldown: 'PARISIENNE'
+                    }]
+                }, {
+                    name: 'PARISIENNE',
+                    id: 'PARISIENNE',
+                    data: [
+							{name: 'PARISIENNE BLEUE',
+							y: 0.60,
+							drilldown: 'PARISIENNE BLEUE'
+							},
+							{name: 'PARISIENNE FILTRE',
+							y: 0.10,
+							drilldown: 'PARISIENNE FILTRE'
+							},
+							{name: 'PARISIENNE CIEL',
+							y: 0.80,
+							drilldown: 'PARISIENNE CIEL'
+							},
+							{name: 'PARISIENNE JAUNE',
+							y: 8.30,
+							drilldown: 'PARISIENNE JAUNE'
+							},
+							{name: 'PARISIENNE OHNE/SANS',
+							y: 4.00,
+							drilldown: 'PARISIENNE OHNE/SANS'
+							},
+							{name: 'PARISIENNE ORANGE',
+							y: 3.50,
+							drilldown: 'PARISIENNE ORANGE'
+							},
+							{name: 'PARISIENNE PETITE',
+							y: 0.20,
+							drilldown: 'PARISIENNE PETITE'
+							},
+							{name: 'PARISIENNE PLUS',
+							y: 0.40,
+							drilldown: 'PARISIENNE PLUS'
+							},
+							{name: 'PARISIENNE ROUGE',
+							y: 0.60,
+							drilldown: 'PARISIENNE ROUGE'
+							}
+                    ]
+                },
+							{
+								name: 'PARISIENNE JAUNE',
+								id: 'PARISIENNE JAUNE',
+								data: [
+										{name: 'PARISIENNE JAUNE KF* C020HL',
+										y: 7.30
+										},
+										{name: 'PARISIENNE JAUNE KF* C020SC',
+										y: 1.00
+										}
+										]
+							},
+				
+				
+				
+				
+				{
+                    name: 'Ph Morris',
+                    id: 'Ph Morris',
+                    data: [
+                        [
+                            'v40.0',
+                            5
+                        ],
+                        [
+                            'v41.0',
+                            4.32
+                        ],
+                        [
+                            'v42.0',
+                            3.68
+                        ],
+                        [
+                            'v39.0',
+                            2.96
+                        ],
+                        [
+                            'v36.0',
+                            2.53
+                        ],
+                        [
+                            'v43.0',
+                            1.45
+                        ],
+                        [
+                            'v31.0',
+                            1.24
+                        ],
+                        [
+                            'v35.0',
+                            0.85
+                        ],
+                        [
+                            'v38.0',
+                            0.6
+                        ],
+                        [
+                            'v32.0',
+                            0.55
+                        ],
+                        [
+                            'v37.0',
+                            0.38
+                        ],
+                        [
+                            'v33.0',
+                            0.19
+                        ],
+                        [
+                            'v34.0',
+                            0.14
+                        ],
+                        [
+                            'v30.0',
+                            0.14
+                        ]
+                    ]
+                }, {
+                    name: 'JTI',
+                    id: 'JTI',
+                    data: [
+                        [
+                            'v35',
+                            2.76
+                        ],
+                        [
+                            'v36',
+                            2.32
+                        ],
+                        [
+                            'v37',
+                            2.31
+                        ],
+                        [
+                            'v34',
+                            1.27
+                        ],
+                        [
+                            'v38',
+                            1.02
+                        ],
+                        [
+                            'v31',
+                            0.33
+                        ],
+                        [
+                            'v33',
+                            0.22
+                        ],
+                        [
+                            'v32',
+                            0.15
+                        ]
+                    ]
+                }, {
+                    name: 'Others',
+                    id: 'Others',
+                    data: [
+                        [
+                            'v8.0',
+                            2.56
+                        ],
+                        [
+                            'v7.1',
+                            0.77
+                        ],
+                        [
+                            'v5.1',
+                            0.42
+                        ],
+                        [
+                            'v5.0',
+                            0.3
+                        ],
+                        [
+                            'v6.1',
+                            0.29
+                        ],
+                        [
+                            'v7.0',
+                            0.26
+                        ],
+                        [
+                            'v6.2',
+                            0.17
+                        ]
+                    ]
+                }]
+            }
+        });
+    });	
 		
 		
 		
