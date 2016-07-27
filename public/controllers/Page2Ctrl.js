ceb6095783ab2eb5c103448d55d5aa24bf430186
shailesh <ADMINIBM@ADMINIB-AUIIJMJ>
2016-07-27 16:21:16 +0530

    changes for new prices screen

--- public/controllers/Page2Ctrl.js
+++ public/controllers/Page2Ctrl.js
@@ -68,8 +68,8 @@ Page2Ctrl.controller('Page2Ctrl', ['$scope','$location','$rootScope','$http',
         tooltip: {
             useHTML: true,
             headerFormat: '<table>',
-            pointFormat: '<tr><th>Brand:</th><td>{point.name}</td></tr>' +
-                '<tr><th>Price:</th><td>{point.y}</td></tr>',
+            pointFormat: '<tr><th>Brand:</th><td> {point.name}</td></tr>' +
+                '<tr><th>Price:</th><td> {point.y}</td></tr>',
             footerFormat: '</table>',
             followPointer: true
         },
